1dc11d6170ee82f7ea829a76d78bd8fb326280f2
wb-zzm298751 <wb-zzm298751@alibaba-inc.com>
2019-01-31 22:02:30 +0800

    init:测试集成antd库的效果

--- src/App.js
+++ src/App.js
@@ -3,18 +3,10 @@ import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.scss';
 import DatePicker from 'antd/lib/date-picker';
-import Button from 'antd/lib/button';
-import Sidebar from './sidebar';
-import Top from './top';
-import Main from './main';
 const mountNode  = document.getElementById('root');
 ReactDOM.render(
     <div className="container"> 
-            <Top content="Hello World"></Top>
-        <div className="wrap">
-            <Sidebar ></Sidebar>
-            <Main></Main>
-        </div>
+        <DatePicker content="Hello World"></DatePicker>
     </div>,
     mountNode
 );
